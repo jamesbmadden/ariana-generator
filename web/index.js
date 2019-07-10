5d3ab4766b4459e530955664be23a1cf3482bab7
James Madden <jamesbmadden@gmail.com>
2019-07-09 19:50:32 -0700

    load the lyrics

--- web/index.js
+++ web/index.js
@@ -3,6 +3,9 @@ import * as tf from '@tensorflow/tfjs';
 async function main () {
   const model = await tf.loadLayersModel('../model/model.json');
   console.log('model loaded');
+  fetch('../python/lyrics.txt')
+    .then(response => response.text())
+    .then(text => console.log(text));
 }
 
 main();
