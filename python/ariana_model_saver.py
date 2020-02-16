cbe593db33dc3bad2fc56cf54175db741b153fb7
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-02-15 17:16:11 -0800

    Rewrite

--- python/ariana_model_saver.py
+++ python/ariana_model_saver.py
@@ -1,24 +1,49 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import tensorflowjs as tfjs
-
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
+import tensorflowjs as tfjs
 
 import numpy as np
 import os
 import time
 
-lyrics_file = open('./lyrics.txt', 'r')
+text = open('./lyrics.txt').read()
 
-text = lyrics_file.read()
-
+# The unique characters in the file
 vocab = sorted(set(text))
 
+# Creating a mapping from unique characters to indices
 char2idx = {u:i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 
-checkpoint_dir = './checkpoints'
+text_as_int = np.array([char2idx[c] for c in text])
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
 # Length of the vocabulary in chars
 vocab_size = len(vocab)
@@ -29,30 +54,22 @@ embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
 
-if tf.test.is_gpu_available():
-  rnn = tf.keras.layers.CuDNNGRU
-else:
-  import functools
-  rnn = functools.partial(
-    tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim,
                               batch_input_shape=[batch_size, None]),
-    rnn(rnn_units,
-        return_sequences=True,
-        recurrent_initializer='glorot_uniform',
-        stateful=True),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
     tf.keras.layers.Dense(vocab_size)
   ])
   return model
 
-model = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=1)
+# Directory where the checkpoints will be saved
+checkpoint_dir = './checkpoints'
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 
