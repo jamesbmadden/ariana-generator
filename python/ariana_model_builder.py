f9ca02a6498239e3f627c440e00d1f49255c33da
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-02-15 16:53:57 -0800

    Rewrite

--- python/ariana_model_builder.py
+++ python/ariana_model_builder.py
@@ -1,16 +1,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
 
 import numpy as np
 import os
 import time
 
-lyrics_file = open('./lyrics.txt', 'r')
+text = open('./lyrics.txt').read()
 
-text = lyrics_file.read()
-
+# The unique characters in the file
 vocab = sorted(set(text))
 
 # Creating a mapping from unique characters to indices
@@ -21,7 +19,7 @@ text_as_int = np.array([char2idx[c] for c in text])
 
 # The maximum length sentence we want for a single input in characters
 seq_length = 100
-examples_per_epoch = len(text)//seq_length
+examples_per_epoch = len(text)//(seq_length+1)
 
 # Create training examples / targets
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
@@ -37,7 +35,6 @@ dataset = sequences.map(split_input_target)
 
 # Batch size
 BATCH_SIZE = 64
-steps_per_epoch = examples_per_epoch//BATCH_SIZE
 
 # Buffer size to shuffle the dataset
 # (TF data is designed to work with possibly infinite sequences,
@@ -46,25 +43,6 @@ steps_per_epoch = examples_per_epoch//BATCH_SIZE
 BUFFER_SIZE = 10000
 
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-
-if tf.test.is_gpu_available():
-  rnn = tf.keras.layers.CuDNNGRU
-else:
-  import functools
-  rnn = functools.partial(
-    tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-  model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                              batch_input_shape=[batch_size, None]),
-    rnn(rnn_units,
-        return_sequences=True,
-        recurrent_initializer='glorot_uniform',
-        stateful=True),
-    tf.keras.layers.Dense(vocab_size)
-  ])
-  return model
 
 # Length of the vocabulary in chars
 vocab_size = len(vocab)
@@ -75,32 +53,37 @@ embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
 
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
 model = build_model(
   vocab_size = len(vocab),
   embedding_dim=embedding_dim,
   rnn_units=rnn_units,
   batch_size=BATCH_SIZE)
 
-for input_example_batch, target_example_batch in dataset.take(1):
-  example_batch_predictions = model(input_example_batch)
-  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
-
 def loss(labels, logits):
   return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
-model.compile(
-    optimizer = tf.train.AdamOptimizer(),
-    loss = loss)
+model.compile(optimizer='adam', loss=loss)
 
 # Directory where the checkpoints will be saved
 checkpoint_dir = './checkpoints'
 # Name of the checkpoint files
-checkpoint_prefix = os.path.join(checkpoint_dir, "ariana_{epoch}")
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
 checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_prefix,
     save_weights_only=True)
 
-EPOCHS=40
-
+EPOCHS=15
 history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
--- python/ariana_model_runner.py
+++ python/ariana_model_runner.py
@@ -1,22 +1,48 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
 
 import numpy as np
 import os
 import time
 
-lyrics_file = open('./lyrics.txt', 'r')
+text = open('./lyrics.txt').read()
 
-text = lyrics_file.read()
-
+# The unique characters in the file
 vocab = sorted(set(text))
 
+# Creating a mapping from unique characters to indices
 char2idx = {u:i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 
-checkpoint_dir = './checkpoints'
+text_as_int = np.array([char2idx[c] for c in text])
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
 # Length of the vocabulary in chars
 vocab_size = len(vocab)
@@ -27,30 +53,22 @@ embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
 
-if tf.test.is_gpu_available():
-  rnn = tf.keras.layers.CuDNNGRU
-else:
-  import functools
-  rnn = functools.partial(
-    tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim,
                               batch_input_shape=[batch_size, None]),
-    rnn(rnn_units,
-        return_sequences=True,
-        recurrent_initializer='glorot_uniform',
-        stateful=True),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
     tf.keras.layers.Dense(vocab_size)
   ])
   return model
 
-model = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=1)
+# Directory where the checkpoints will be saved
+checkpoint_dir = './checkpoints'
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 
@@ -72,7 +90,7 @@ def generate_text(model, start_string):
   # Low temperatures results in more predictable text.
   # Higher temperatures results in more surprising text.
   # Experiment to find the best setting.
-  temperature = 1.0
+  temperature = 0.5
 
   # Here batch size == 1
   model.reset_states()
@@ -81,11 +99,11 @@ def generate_text(model, start_string):
       # remove the batch dimension
       predictions = tf.squeeze(predictions, 0)
 
-      # using a multinomial distribution to predict the word returned by the model
+      # using a categorical distribution to predict the character returned by the model
       predictions = predictions / temperature
       predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
 
-      # We pass the predicted word as the next input to the model
+      # We pass the predicted character as the next input to the model
       # along with the previous hidden state
       input_eval = tf.expand_dims([predicted_id], 0)
 
@@ -93,4 +111,4 @@ def generate_text(model, start_string):
 
   return (start_string + ''.join(text_generated))
 
-print(generate_text(model, start_string=u"[Chorus"))
+print(generate_text(model, start_string=u"[Verse 1"))
